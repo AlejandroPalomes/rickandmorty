2cf820f7be091cb11e13182150d2a33d719a2620
Alex Palomes <alejandropalomes@hotmail.com>
2020-05-30 13:03:21 +0200

    max with menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
 var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene, 
+var parallaxInstance = new Parallax(scene
+    , 
 //     {
 //   relativeInput: true
 // }
 );
 parallaxInstance.friction(0.2, 0.2);
+
+$("#sideMenu").mouseover(e=>{
+    console.log(e.target)
+    $("#sideMenu").stop().animate({
+        width: "25%",
+    }, 500, "swing")
+    // $("#sideMenu").css("width", "400px")
+})
+
+$("#sideMenu").mouseout(e=>{
+    console.log("hover works")
+    $("#sideMenu").stop().animate({
+        width: "20px"
+    }, 500)
+    // $("#sideMenu").css("width", "20px")
+})
