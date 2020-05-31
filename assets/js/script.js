fc6590e0c1cd9a1b094344b91d74e28580fe57cf
Alex Palomes <alejandropalomes@hotmail.com>
2020-05-31 03:56:08 +0200

    mobile side bar menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,6 @@ var parallaxInstance = new Parallax(scene
 );
 parallaxInstance.friction(0.2, 0.2);
 
-$("#sideMenu").mouseover(e=>{
-    $("#sideMenu").stop().animate({
-        width: "25%",
-    }, 500, "swing")
-    // $("#sideMenu").css("width", "400px")
-})
-
-$("#sideMenu").mouseout(e=>{
-    $("#sideMenu").stop().animate({
-        width: "20px"
-    }, 500)
-    // $("#sideMenu").css("width", "20px")
-})
-
 
 axios({
     method: 'get',
@@ -29,7 +15,7 @@ axios({
   }).then((response)=>{
     $(response.data.results).each((i,e)=>{
         $("#episodes").append(`<li><button id="${e.id}" data-url="${e.url}" class="main__container__menu--episode mb-1 p-2">${e.name}</button><li>`);
-        $("#episodes-sm").append(`<li><button id="${e.id}" data-url="${e.url}" class="main__container__menu--episode mb-1 p-2 dropdown-item">${e.name}</button><li>`);
+        $("#episodes-sm").append(`<li><button id="${e.id}" data-url="${e.url}" class="main__container__menu--episode mb-1 p-2" data-toggle="collapse" data-target="#collapseMenu" aria-controls="collapseMenu" aria-expanded="false" aria-label="Toggle navigation">${e.name}</button><li>`);
     })
     $(".main__container__menu--episode").click(e=>{
         printEpisode($(e.target).data("url"));
